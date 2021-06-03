3b15f37d5bee22cd1d9a2efea60eee2a1ebf1649
Hugh Chen <hughchen@google.com>
2021-06-03 16:38:24 +0800

    RESTRICT AUTOMERGE Fix phishing attacks over Bluetooth due to unclear warning message
    This CL add more prompts presented for users to avoid phishing attacks.
    
    Screenshot:
    https://screenshot.googleplex.com/p5PZbphN46ddPFV.png
    https://screenshot.googleplex.com/6Q2wKfPbNQmTtx8.png
    https://screenshot.googleplex.com/987VpYgNUZL2K4T.png
    https://screenshot.googleplex.com/9eVg6SAGScVXU8U.png
    
    Bug: 167403112
    Test: manually test
    Change-Id: Iadec059b662fd91754ad573bbe688702cdd3c9af
    (cherry picked from commit 10e459921953825d34e70cc4da846aac703d913c)
    (cherry picked from commit a5046f219a545246f244f0dc003eefdeb1dfeb93)
    (cherry picked from commit a7e6aafcd25c504671804e4b3f1133c5ef83dca4)

--- src/com/android/settings/bluetooth/BluetoothPermissionActivity.java
+++ src/com/android/settings/bluetooth/BluetoothPermissionActivity.java
@@ -134,9 +134,14 @@ public class BluetoothPermissionActivity extends AlertActivity implements
             p.mView = createSapDialogView();
             break;
         }
-        p.mPositiveButtonText = getString(R.string.allow);
+        p.mPositiveButtonText = getString(
+                requestType == BluetoothDevice.REQUEST_TYPE_PROFILE_CONNECTION
+                        ? R.string.bluetooth_connect_access_dialog_positive : R.string.allow);
         p.mPositiveButtonListener = this;
-        p.mNegativeButtonText = getString(R.string.request_manage_bluetooth_permission_dont_allow);
+        p.mNegativeButtonText = getString(
+                requestType == BluetoothDevice.REQUEST_TYPE_PROFILE_CONNECTION
+                        ? R.string.bluetooth_connect_access_dialog_negative
+                        : R.string.request_manage_bluetooth_permission_dont_allow);
         p.mNegativeButtonListener = this;
         mOkButton = mAlert.getButton(DialogInterface.BUTTON_POSITIVE);
         setupAlert();
