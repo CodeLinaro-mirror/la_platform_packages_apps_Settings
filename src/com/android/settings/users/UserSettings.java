e58be9f8f7738fbb3fe3ab20b1a4c585d276636e
Tetiana Meronyk <tetianameronyk@google.com>
2022-08-22 17:52:03 +0000

    Remove overflow from guest user settings.
    - Removed overflow for guest users
    - Code cleanup
    
    Bug: 191509236
    Test: atest UserSettingsTest
    (cherry picked from commit 53a45bea3de9befde2a88e76cc50019ade8bf19c)
    Change-Id: I1ccf9924a471f148bf6413a040ccc769f9293c97

--- src/com/android/settings/users/UserSettings.java
+++ src/com/android/settings/users/UserSettings.java
@@ -274,7 +274,7 @@ public class UserSettings extends SettingsPreferenceFragment
         final SettingsActivity activity = (SettingsActivity) getActivity();
         final SettingsMainSwitchBar switchBar = activity.getSwitchBar();
         switchBar.setTitle(getContext().getString(R.string.multiple_users_main_switch_title));
-        if (mUserCaps.mIsAdmin) {
+        if (isCurrentUserAdmin()) {
             switchBar.show();
         } else {
             switchBar.hide();
@@ -340,7 +340,7 @@ public class UserSettings extends SettingsPreferenceFragment
         mMePreference = new UserPreference(getPrefContext(), null /* attrs */, myUserId);
         mMePreference.setKey(KEY_USER_ME);
         mMePreference.setOnPreferenceClickListener(this);
-        if (mUserCaps.mIsAdmin) {
+        if (isCurrentUserAdmin()) {
             mMePreference.setSummary(R.string.user_admin);
         }
 
@@ -429,10 +429,7 @@ public class UserSettings extends SettingsPreferenceFragment
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         int pos = 0;
-        // TODO(b/191509236): The menu item does not need to be accessible for guest users,
-        //  regardless of mGuestUserAutoCreated
-        if (!mUserCaps.mIsAdmin && canSwitchUserNow() && !(isCurrentUserGuest()
-                && mGuestUserAutoCreated)) {
+        if (!isCurrentUserAdmin() && canSwitchUserNow() && !isCurrentUserGuest()) {
             String nickname = mUserManager.getUserName();
             MenuItem removeThisUser = menu.add(0, MENU_REMOVE_USER, pos++,
                     getResources().getString(R.string.user_remove_user_menu, nickname));
@@ -1191,12 +1188,12 @@ public class UserSettings extends SettingsPreferenceFragment
         // don't show the guest user icon, instead we show two preferences for guest user to
         // exit and reset itself. Hence we don't add mMePreference, i.e. guest user to the
         // list of users visible in the UI.
-        if (!mUserCaps.mIsGuest) {
+        if (!isCurrentUserGuest()) {
             userPreferences.add(mMePreference);
         }
 
         boolean canOpenUserDetails =
-                mUserCaps.mIsAdmin || (canSwitchUserNow() && !mUserCaps.mDisallowSwitchUser);
+                isCurrentUserAdmin() || (canSwitchUserNow() && !mUserCaps.mDisallowSwitchUser);
         for (UserInfo user : users) {
             if (user.isGuest()) {
                 // Guest user is added to guest category via updateGuestCategory
@@ -1317,6 +1314,10 @@ public class UserSettings extends SettingsPreferenceFragment
         return mUserCaps.mIsGuest;
     }
 
+    private boolean isCurrentUserAdmin() {
+        return mUserCaps.mIsAdmin;
+    }
+
     private boolean canSwitchUserNow() {
         return mUserManager.getUserSwitchability() == UserManager.SWITCHABILITY_STATUS_OK;
     }
@@ -1364,7 +1365,7 @@ public class UserSettings extends SettingsPreferenceFragment
         UserPreference pref = null;
         boolean isGuestAlreadyCreated = false;
         boolean canOpenUserDetails =
-                mUserCaps.mIsAdmin || (canSwitchUserNow() && !mUserCaps.mDisallowSwitchUser);
+                isCurrentUserAdmin() || (canSwitchUserNow() && !mUserCaps.mDisallowSwitchUser);
 
         mGuestUserCategory.removeAll();
         mGuestUserCategory.setVisible(false);
@@ -1412,7 +1413,7 @@ public class UserSettings extends SettingsPreferenceFragment
             // "reset guest on exit" preference is shown hence also make guest category visible
             mGuestUserCategory.setVisible(true);
         }
-        if (mUserCaps.mIsGuest) {
+        if (isCurrentUserGuest()) {
             // guest category is not visible for guest user.
             mGuestUserCategory.setVisible(false);
         }
