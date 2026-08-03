221ace2df7d9808f0cc4aa5783ca39386368f42b
Mengjun Leng <mengjunl@qti.qualcomm.com>
2026-08-03 11:32:06 +0530

    Markings fix in CallStateRepository.kt
    Change-Id: I8a1ac897a57aa201df00b1129270c82a60fe6782
    CRs-Fixed: 4630680

--- src/com/android/settings/network/telephony/CallStateRepository.kt
+++ src/com/android/settings/network/telephony/CallStateRepository.kt
@@ -15,9 +15,9 @@
  */
 
 /*
+ * Changes from Qualcomm Technologies, Inc. are provided under the following license:
  * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
- * All rights reserved.
- * Confidential and Proprietary - Qualcomm Technologies, Inc.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
 package com.android.settings.network.telephony
